088f9eae74119464e5b5f08e2a708533eaeb8ba0
Sai Kambampati <ssv1065@gmail.com>
2022-04-06 07:59:08 -0700

    Added Xcode Project

---
+++ Athena/Athena/AthenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthenaApp.swift
+//  Athena
+//
+//  Created by Sai Kambampati on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AthenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Athena/Athena/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Athena
+//
+//  Created by Sai Kambampati on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
