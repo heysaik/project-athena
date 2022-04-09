2da76f0c0b3242fab24712cdfa93dea9b93aaa06
Sai Kambampati <ssv1065@gmail.com>
2022-04-09 12:49:41 -0700

    Created onboarding 1

--- Athena/Athena/ContentView.swift
+++ Athena/Athena/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Welcome to Athena")
-            .padding()
+        VStack(alignment: .leading, spacing: 8) {
+            Spacer()
+            Image(systemName: "book.fill")
+                .font(.system(size: 50))
+            Text("Welcome to Athena")
+                .font(.system(.title, design: .rounded)).bold()
+            Text("Athena is the app for book lovers around the world to help you keep track of your readings.")
+                .font(.system(.title3, design: .rounded))
+            Button("Next") {
+                // Go to next screen
+            }
+        }
+        .padding()
     }
 }
 
