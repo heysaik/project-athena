2440acd7885e0da41e64c16537698cb34bcf8d01
Logan Thompson <lorthomp@ucsc.edu>
2022-04-14 19:02:01 -0700

    Deleted the NavigationView and commented out the NavigationLink on OnboardingThree

--- Athena/Athena/OnboardingThree.swift
+++ Athena/Athena/OnboardingThree.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct OnboardingThree: View {
     var body: some View {
-        NavigationView{
         ZStack {
             LinearGradient(colors: [Color(.displayP3, red: 0, green: 145/255, blue: 1, opacity: 1.0), Color(.displayP3, red: 0, green: 68/255, blue: 215/255, opacity: 1.0)], startPoint: .topLeading, endPoint: .center)
                 .edgesIgnoringSafeArea(.all)
@@ -28,8 +27,8 @@ struct OnboardingThree: View {
                         .foregroundColor(.white)
                 }
                 .padding(.horizontal)
-                
-                NavigationLink(destination: OnboardingFour().navigationBarHidden(true)){
+                //Uncomment out NavigationLink and the ending curly braces to link to the next view
+                //NavigationLink(destination: OnboardingFour().navigationBarHidden(true)){
                     ZStack {
                         RoundedRectangle(cornerRadius: 22)
                             .frame(height: 44)
@@ -38,12 +37,11 @@ struct OnboardingThree: View {
                             .font(.system(size: 17, weight: .semibold, design: .rounded))
                             .foregroundColor(.white)
                     }
-                }
+                //}
                 .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
                 .padding(.horizontal)
             }
             .padding(.vertical)
-        }
         }
     }
 }
