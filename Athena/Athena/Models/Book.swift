aab745930e8b4a2c1efb37ab861b4be645c6660a
Sai Kambampati <ssv1065@gmail.com>
2022-04-19 22:16:38 -0700

    Created Detail View

--- Athena/Athena/Models/Book.swift
+++ Athena/Athena/Models/Book.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // This structure is used to save 
-struct Book: Identifiable, Codable {
+struct Book: Identifiable, Convertable, Equatable {
     var id: String
     var title: String
     var authors: [String]
@@ -20,4 +20,20 @@ struct Book: Identifiable, Codable {
     var isbn13: String?
     var isbn10: String?
     var imageLink: String
+}
+
+protocol Convertable: Codable {}
+extension Convertable {
+    func convertToDict() -> Dictionary<String, Any>? {
+        var dict: Dictionary<String, Any>? = nil
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(self)
+            dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Dictionary<String, Any>
+        } catch {
+            print(error)
+        }
+
+        return dict
+    }
 }
