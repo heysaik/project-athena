a0c45bdbbef79f4003aa6566be0a796eb32d4ae8
Kevin Crawford <kemcrawf@Kevins-MacBook-Air.local>
2022-04-19 19:55:08 -0700

    Profile view now shows email of current user

--- Athena/Athena/Views/ProfileView.swift
+++ Athena/Athena/Views/ProfileView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
 
 struct ProfileView: View {
+    @State private var email = Auth.auth().currentUser!.email
+    
     var body: some View {
         ZStack {
             LinearGradient(colors: [Color(.displayP3, red: 0, green: 145/255, blue: 1, opacity: 1.0), Color(.displayP3, red: 0, green: 68/255, blue: 215/255, opacity: 1.0)], startPoint: .topLeading, endPoint: .center)
@@ -16,7 +20,7 @@ struct ProfileView: View {
                 Text("Profile")
                     .font(.system(size:40, design: .rounded)).bold()
                     .foregroundColor(.white)
-                Text("email")
+                Text(email!)
                     .font(.system(size: 25, design: .rounded))
                     .foregroundColor(.white)
                 ZStack{
