7019c8093d0a9376348c52de9a1e58eefaa45db6
Sai Kambampati <ssv1065@gmail.com>
2022-05-16 16:19:50 -0700

    Edit name and email

--- Athena/Athena/Views/SettingsView.swift
+++ Athena/Athena/Views/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SDWebImage
 import FirebaseAuth
 import FirebaseFirestore
+import AlertKit
 
 struct SettingsView: View {
     
@@ -17,11 +18,22 @@ struct SettingsView: View {
     @State private var showDeleteAlert = false
     @State private var showEmailAlert = false
     @State private var loggedOut = false
-    
+
+    @State private var newName = ""
+    @State private var newEmail = ""
+    @State private var username = ""
+    @State private var password = ""
+    @StateObject var nameAlertManager = CustomAlertManager()
+    @StateObject var emailAlertManager = CustomAlertManager()
+    @StateObject var authAlertManager = CustomAlertManager()
+
     // Used to show the gradient
     init() {
         UITableView.appearance().backgroundColor = .clear
     }
+    
+    private let firestore = Firestore.firestore()
+    private let auth = Auth.auth()
     
     var body: some View {
         NavigationView {
@@ -53,6 +65,7 @@ struct SettingsView: View {
                         HStack {
                             Button {
                                 // Edit Name
+                                nameAlertManager.show()
                             } label: {
                                 Label("Edit Name", systemImage: "person.fill")
                                     .font(.system(size: 17, design: .rounded))
@@ -60,9 +73,9 @@ struct SettingsView: View {
                             }
                         }
                         HStack {
-                            NavigationLink {
+                            Button {
                                 // Edit Email
-                                Change_Email()
+                                authAlertManager.show()
                             } label: {
                                 Label("Edit Email", systemImage: "envelope.fill")
                                     .font(.system(size: 17, design: .rounded))
@@ -72,8 +85,8 @@ struct SettingsView: View {
                             Button {
                                 // Change Password
                                 do{
-                                    Auth.auth().sendPasswordReset(withEmail: Auth.auth().currentUser!.email!) { error in
-                                        if error == nil{
+                                    auth.sendPasswordReset(withEmail: auth.currentUser!.email!) { error in
+                                        if error == nil {
                                             showEmailAlert = true
                                         }
                                     }
@@ -83,14 +96,14 @@ struct SettingsView: View {
                                     .font(.system(size: 17, design: .rounded))
                             }
                         }
-                        .alert("An email to change your password has been sent to \(Auth.auth().currentUser!.email!)", isPresented: $showEmailAlert){
+                        .alert("An email to change your password has been sent to \(auth.currentUser!.email!)", isPresented: $showEmailAlert){
                             Button("OK") {
                                 showEmailAlert = false
                             }
                         }
                     }
                     .listRowBackground(Color.white.opacity(0.2))
-
+                    
                     Section(header: Label("More Actions", systemImage: "ellipsis")) {
                         HStack {
                             Button {
@@ -118,10 +131,10 @@ struct SettingsView: View {
                             } label: {
                                 Text("Cancel")
                             }
-
-                            Button(role: .cancel) {
+                            
+                            Button(role: .destructive) {
                                 do {
-                                    try Auth.auth().signOut()
+                                    try auth.signOut()
                                     loggedOut = true
                                 } catch {
                                     print("already logged out")
@@ -141,72 +154,71 @@ struct SettingsView: View {
                                 
                             }
                         }
-                        .alert("Are you sure you want to log out?", isPresented: $showDeleteAlert) {
-                            
+                        .alert("Are you sure?", isPresented: $showDeleteAlert, actions: {
                             Button(role: .cancel) {
                                 showDeleteAlert.toggle()
                             } label: {
                                 Text("Cancel")
                             }
-
+                            
                             Button(role: .destructive) {
                                 Task {
-                                    if let userID = Auth.auth().currentUser?.uid {
+                                    if let userID = auth.currentUser?.uid {
                                         // Delete Firestore Public Data
-                                        try await Firestore.firestore()
+                                        try await firestore
                                             .collection("users")
                                             .document(userID)
                                             .delete()
                                         
                                         // Delete Firestore Private Data
-                                        try await Firestore.firestore()
+                                        try await firestore
                                             .collection("private")
                                             .document(userID)
                                             .delete()
                                         
                                         // Delete Notes
-                                        let notesDocs = try await Firestore.firestore()
+                                        let notesDocs = try await firestore
                                             .collection("notes")
                                             .whereField("creatorID", isEqualTo: userID)
                                             .getDocuments()
                                         for document in notesDocs.documents {
-                                            try await Firestore.firestore()
+                                            try await firestore
                                                 .collection("notes")
                                                 .document(document.documentID)
                                                 .delete()
                                         }
                                         
                                         // Delete Already Read
-                                        let arDocs = try await Firestore.firestore()
+                                        let arDocs = try await firestore
                                             .collection("alreadyRead")
                                             .whereField("readerID", isEqualTo: userID)
                                             .getDocuments()
                                         for document in arDocs.documents {
-                                            try await Firestore.firestore()
+                                            try await firestore
                                                 .collection("alreadyRead")
                                                 .document(document.documentID)
                                                 .delete()
                                         }
                                         
                                         // Wishlist Notes
-                                        let wishDocs = try await Firestore.firestore()
+                                        let wishDocs = try await firestore
                                             .collection("wishlist")
                                             .whereField("readerID", isEqualTo: userID)
                                             .getDocuments()
                                         for document in wishDocs.documents {
-                                            try await Firestore.firestore()
+                                            try await firestore
                                                 .collection("notes")
                                                 .document(document.documentID)
                                                 .delete()
                                         }
                                         
                                         // Currently Reading Notes
-                                        let crDocs = try await Firestore.firestore()
+                                        let crDocs = try await firestore
                                             .collection("currentlyReading")
                                             .whereField("creatorID", isEqualTo: userID)
                                             .getDocuments()
                                         for document in crDocs.documents {
-                                            try await Firestore.firestore()
+                                            try await firestore
                                                 .collection("currentlyReading")
                                                 .document(document.documentID)
                                                 .delete()
@@ -214,8 +226,8 @@ struct SettingsView: View {
                                         
                                         // Delete Auth
                                         do {
-                                            try Auth.auth().signOut()
-                                            try await Auth.auth().currentUser?.delete()
+                                            try auth.signOut()
+                                            try await auth.currentUser?.delete()
                                             loggedOut = true
                                         } catch {
                                             print("already logged out")
@@ -228,11 +240,111 @@ struct SettingsView: View {
                             } label: {
                                 Text("Delete")
                             }
-                        }
+                        }, message: {
+                            Text("This will delete all of your notes, books in your library, and data off of our servers. This action is irreversible.")
+                        })
                     }
                     .listRowBackground(Color.white.opacity(0.2))
-
                 }
+                .customAlert(manager: nameAlertManager, content: {
+                    VStack {
+                        Text("Would you like to change your name?").bold()
+                        Text("This change may take a few seconds to propagate across our servers.")
+                        TextField("Bruce Wayne", text: $newName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .textContentType(.name)
+                    }
+                }, buttons: [
+                    .cancel(content: {
+                        Text("Cancel")
+                    }),
+                    .regular(content: {
+                        Text("Update")
+                    }, action: {
+                        if let userID = auth.currentUser?.uid {
+                            Firestore.firestore()
+                                .collection("users")
+                                .document(userID)
+                                .updateData([
+                                    "name": newName
+                                ], completion: { error in
+                                    if error == nil {
+                                        print("success")
+                                    } else {
+                                        print("ERROR CHNAGNG", error!.localizedDescription)
+                                    }
+                                })
+                        } else {
+                            print("no uid")
+                        }
+                    })
+                ])
+                .customAlert(manager: authAlertManager, content: {
+                    VStack {
+                        Text("Please reauthenticate your account").bold()
+                        Text("To change your email, please login again.")
+                        TextField("Bruce Wayne", text: $username)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .textContentType(.emailAddress)
+                        SecureField("Password", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .textContentType(.password)
+                    }
+                }, buttons: [
+                    .cancel(content: {
+                        Text("Cancel")
+                    }),
+                    .regular(content: {
+                        Text("Login")
+                    }, action: {
+                        let credential = EmailAuthProvider.credential(withEmail: username, password: password)
+                        
+                        Task {
+                            do {
+                                if let user = auth.currentUser {
+                                    try await user.reauthenticate(with: credential)
+                                    
+                                    emailAlertManager.show()
+                                }
+                            } catch let error {
+                                print(error.localizedDescription)
+                            }
+                        }
+                    })
+                ])
+                .customAlert(manager: emailAlertManager, content: {
+                    VStack {
+                        Text("Would you like to change your email?").bold()
+                        Text("All future Athena communications will be sent to this email.")
+                        TextField("bruce@wayne.com", text: $newEmail)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .textContentType(.emailAddress)
+                    }
+                }, buttons: [
+                    .cancel(content: {
+                        Text("Cancel")
+                    }),
+                    .regular(content: {
+                        Text("Update")
+                    }, action: {
+                        Task {
+                            do {
+                                if let user = auth.currentUser {
+                                    try await user.updateEmail(to: newEmail)
+                                    
+                                    try await firestore
+                                        .collection("private")
+                                        .document(user.uid)
+                                        .updateData([
+                                            "email": newEmail
+                                        ])
+                                }
+                            } catch let error {
+                                print(error.localizedDescription)
+                            }
+                        }
+                    })
+                ])
             }
             .navigationTitle("Settings")
             .toolbar {
@@ -248,6 +360,25 @@ struct SettingsView: View {
         .fullScreenCover(isPresented: $loggedOut) {
             // Shows OnboardingOne if the user logs out
             OnboardingOne()
+        }
+        .onAppear {
+            if let userID = auth.currentUser?.uid {
+                Task {
+                    let publicDoc = try await firestore
+                        .collection("users")
+                        .document(userID)
+                        .getDocument()
+                    let publicData = publicDoc.data()
+                    self.newName = publicData?["name"] as? String ?? ""
+                    
+                    let privateDoc = try await firestore
+                        .collection("private")
+                        .document(userID)
+                        .getDocument()
+                    let privateData = privateDoc.data()
+                    self.newEmail = privateData?["email"] as? String ?? ""
+                }
+            }
         }
     }
 }
